357b3e55bf56798610f7891ccdf3293a737c2490
mumbaiops45 <mumbaiops45@gmail.com>
2026-08-19 17:46:34 +0530

    make some thingd

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // app.use(cors());
 app.use(
     cors({
-        origin: "https://waterpurifier-1.onrender.com",
+        origin: "https://vrs-black.vercel.app",
         credentials: true,
     })
 );
